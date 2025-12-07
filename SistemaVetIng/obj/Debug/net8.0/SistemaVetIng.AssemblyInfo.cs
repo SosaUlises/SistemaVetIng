d58a9da7f1b9d62e765d2ccd663f9068b29484df
SosaUlises <ulipvp@gmail.com>
2025-12-07 13:38:53 -0300

    renombrando carpeta imagenes por problemas con casesentitive

--- SistemaVetIng/obj/Debug/net8.0/SistemaVetIng.AssemblyInfo.cs
+++ SistemaVetIng/obj/Debug/net8.0/SistemaVetIng.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaVetIng")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6cd2399b4f395140d693b05c1112cde21c2dfe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0620023cb5f9cf1f29592d556cd71c36f4a81cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaVetIng")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaVetIng")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
